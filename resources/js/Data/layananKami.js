56593c748c2529cb7046c5989d3c0994c8d29ebb
faracutrisfa <faracutrisfa@gmail.com>
2024-11-18 17:13:48 +0700

    feat: add layanan components for landing page

---
+++ resources/js/Data/layananKami.js
@@ -0,0 +1,24 @@
+const layananKami = [
+    {
+        icon: "./build/assets/LandingPage/layanan1.webp",
+        title: "Layanan Ikan Segar",
+        description: "Menyediakan ikan segar langsung dari nelayan terpercaya, memastikan kualitas dan kesegarannya sampai di tangan kamu. Setiap produk dipilih dengan cermat untuk memenuhi kebutuhan protein hewani yang sehat dan lezat."
+    },
+    {
+        icon: "./build/assets/LandingPage/layanan2.webp",
+        title: "Pesan Antar",
+        description: "Nikmati kemudahan belanja ikan segar tanpa harus keluar rumah. Layanan pesan antar kami memastikan produk sampai dengan cepat dan dalam kondisi terbaik, langsung ke depan pintu kamu."
+    },
+    {
+        icon: "./build/assets/LandingPage/layanan3.webp",
+        title: "Edukasi",
+        description: "Pelajari lebih lanjut tentang manfaat ikan bagi kesehatan, tips memilih ikan yang berkualitas, hingga resep-resep sehat dan mudah. Kami berkomitmen untuk meningkatkan kesadaran akan pentingnya konsumsi ikan dalam kehidupan sehari-hari."
+    },
+    {
+        icon: "./build/assets/LandingPage/layanan4.webp",
+        title: "Mitra Tetap",
+        description: "Bersama mitra-mitra nelayan lokal, kami menjamin pasokan ikan yang konsisten dan berkualitas. Kemitraan ini juga mendukung perekonomian lokal, sekaligus menjaga keberlanjutan ekosistem laut."
+    },
+]
+
+export default layananKami;
