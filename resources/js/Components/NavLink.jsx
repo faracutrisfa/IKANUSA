4068bf3386cfd6e30d13408aa2ae8d9e4d99399c
faracutrisfa <faracutrisfa@gmail.com>
2024-11-19 20:45:09 +0700

    fix: fix navlink component

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -1,21 +1,23 @@
-import { Link } from '@inertiajs/react';
-import { cn } from '../Utils/cn'; 
+import { Link, usePage } from '@inertiajs/react';
+import { cn } from '../Utils/cn';
 
 export default function NavLink({
-    active = false,
     className = '',
     children,
     ...props
 }) {
+    const { url } = usePage(); 
+    const isActive = url === props.href;
+
     return (
         <Link
             {...props}
             className={cn(
                 'inline-flex items-center border-b-2 px-1 pt-1 text-center text-sm leading-5 transition duration-150 ease-in-out focus:outline-none',
-                active
+                isActive
                     ? 'border-normal-blue-active text-darker-blue font-semibold focus:border-normal-blue-hover'
                     : 'border-transparent text-darker-blue hover:border-normal-blue-hover hover:text-dark-blue-active focus:border-normal-blue-hover focus:text-dark-blue-active',
-                className 
+                className
             )}
         >
             {children}
