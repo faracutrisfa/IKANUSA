9852df18a26e705458e1a6b68a262e1cbc33e9e8
faracutrisfa <faracutrisfa@gmail.com>
2024-11-19 20:41:36 +0700

    style: add style NotPage component

--- resources/js/Components/NotPage.jsx
+++ resources/js/Components/NotPage.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const NotPage = () => {
     return (
         <section>
-            INI NOT PAGE
+            <div className='flex flex-col items-center justify-center h-screen gap-10 -mt-10'>
+                <h1 className='text-xl lg:text-2xl text-dark-blue-active font-semibold animate-bounce'>HALAMAN MASIH DIPANCING</h1>
+                <img src="./build/assets/LandingPage/NotFound.webp" alt="image" className='w-80 rounded-xl' />
+            </div>
         </section>
     )
 }
