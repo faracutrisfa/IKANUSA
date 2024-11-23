b6bc0e95302fede8bf67eaae63a32027caac2be9
faracutrisfa <faracutrisfa@gmail.com>
2024-11-23 10:28:41 +0700

    feat: update assets

--- resources/js/Components/NotPage.jsx
+++ resources/js/Components/NotPage.jsx
@@ -5,7 +5,7 @@ const NotPage = () => {
         <section>
             <div className='flex flex-col items-center justify-center h-screen gap-10 -mt-10'>
                 <h1 className='text-xl lg:text-2xl text-dark-blue-active font-semibold animate-bounce'>HALAMAN MASIH DIPANCING</h1>
-                <img src="./build/assets/LandingPage/NotFound.webp" alt="image" className='w-80 rounded-xl' />
+                <img src="./assets/LandingPage/NotFound.webp" alt="image" className='w-80 rounded-xl' />
             </div>
         </section>
     )
