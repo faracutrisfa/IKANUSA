58157c13f09b1b8d983395821e8d76eb8144ed9b
faracutrisfa <faracutrisfa@gmail.com>
2024-11-25 18:37:59 +0700

    fix: fixed landing page layout

--- resources/js/Layouts/LandingPageLayout.jsx
+++ resources/js/Layouts/LandingPageLayout.jsx
@@ -2,10 +2,10 @@ import Footer from '@/Components/Footer'
 import Navbar from '@/Components/Navbar'
 import React from 'react'
 
-const LandingPageLayout = ({ children }) => {
+const LandingPageLayout = ({ children, auth, laravelVersion, phpVersion }) => {
     return (
         <div className='font-poppins'>
-            <Navbar />
+            <Navbar auth={auth}/>
             <div className='pt-[87px]'>
                 {children}
             </div>
